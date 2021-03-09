9506524effd167aedabdb8004653172c7f8b45bf
TotoNezo <antoine.droyer@gmail.com>
2021-03-09 16:24:22 +0100

    Update script.js

--- dist/script.js
+++ dist/script.js
@@ -3,13 +3,22 @@
 $(document).ready(function() {
     $("#jouer").hide();
     $(document).on("scroll",function(){
+        var target=$(".block2").offset().top;
+        console.log(target);
         var position=$(window).scrollTop();
         console.log(position);
         //var scrollValue=$(target).offset().top;
-      if (position < 200){
+      if (position < target - 100){
         $("#jouer").hide();
-      }else if (position > 200){
+      }else if (position > target - 100){
           $("#jouer").show();
+      }
+      var ScrollValueBlur=$(".blureffect").offset().top;
+      console.log(ScrollValueBlur);
+      if (position > ScrollValueBlur - 100){
+        $(".logo").css({"filter":"blur(4px)"})
+      }else if (position < ScrollValueBlur - 100){
+        $(".logo").css({"filter":"blur(0px)"})
       }
        });
 });
